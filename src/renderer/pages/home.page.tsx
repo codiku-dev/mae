6dba5a9234059792a679883da6718798ef5e8328
rlebhar-clone <robin.lebhar@getclone.io>
2024-09-14 15:20:03 +0200

    Update home.page.tsx

--- src/renderer/pages/home.page.tsx
+++ src/renderer/pages/home.page.tsx
@@ -100,6 +100,7 @@ export function HomePage() {
       setIsStreamingFinished(false);
       setValue('');
       setError('');
+
       if (!getCurrentConversation()) {
         await createNewConversation(submittedText.slice(0, 30) + '...');
       }
