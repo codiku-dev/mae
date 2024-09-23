285b441484f6e6bb39dc15abe4c296edef76dd23
rlebhar-clone <robin.lebhar@getclone.io>
2024-09-23 21:32:32 +0200

    wip

--- src/main/window.ts
+++ src/main/window.ts
@@ -5,7 +5,7 @@ import { start } from './start';
 export function initWindow() {
   // const { width, height } = screen.getPrimaryDisplay().workAreaSize;
   // global.DEBUG = !app.isPackaged;
-  global.DEBUG = true;
+  global.DEBUG = false;
   let mainWindow: BrowserWindow | null = new BrowserWindow({
     height: screen.getPrimaryDisplay().workAreaSize.height,
     width: screen.getPrimaryDisplay().workAreaSize.width,
