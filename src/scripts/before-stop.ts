3d3ba0e683e7924a67daee00d85074f72c2a8834
rlebhar-clone <robin.lebhar@getclone.io>
2024-10-05 15:27:56 +0200

    rm

--- src/scripts/before-stop.ts
+++
@@ -1,5 +0,0 @@
-import { ollamaService } from '@/main/modules/ollama/ollama.service';
-
-export async function beforeStop() {
-  await ollamaService.stop();
-}
--- src/scripts/run-before-stop.ts
+++
@@ -1,3 +0,0 @@
-import { beforeStop } from './before-stop';
-
-beforeStop();
